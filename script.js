0a23dd3a45a833a1186dc9136448ea027fe2f695
Florent-Vasseur <vasseurflorent@gmail.com>
2022-09-23 00:04:50 +0200

    V1 is functionnal

--- script.js
+++ script.js
@@ -3,9 +3,21 @@
 const gameController = ((name1, name2) => {
     let round = 1;
 
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
     const createPlayer = (name, sign, codeSign) => {
         let score = 0;
-        return {name, sign, codeSign, score}
+        let choice = [];
+        return {name, sign, codeSign, score, choice}
     };
 
     const createBoard = () => {
@@ -19,56 +31,92 @@ const gameController = ((name1, name2) => {
     };
 
     const playround = (indexBox) => {
-        console.log(indexBox);
-        let sign;
-        let codeSign;
-        if (round%2 === 1) {
-            sign = player1.sign;
-            codeSign = player1.codeSign;
+        console.log(board[indexBox]==="");
+        if (board[indexBox]==="") {
+            let player = round%2 ===1 ? playerX : playerO;
+            let codeSign = storeChoice(indexBox, player);
+            let winner = isWinner(player.choice)
+            round++;
+            let nextPlayer = round%2 ===1 ? playerX : playerO;
+            console.log({codeSign, winner, nextPlayer})
+            return {codeSign, winner, nextPlayer};
         } else {
-            sign = player2.sign;
-            codeSign = player2.codeSign;
+            return false;
         }
-        console.log("round numéro", round);
-        console.log(sign);
-        console.log(codeSign);
-        round++;
 
     };
 
-    const fillBoard = (pos, player) => {
-        board[pos] = player.sign;
+    const storeChoice = (index, player) => {
+        board[index]=player.sign;
+        player.choice.push(index);
+        return player.codeSign;
     };
 
+    const isWinner = choice => {
+        let flag = false;
+        winConditions.forEach( win => {
+            if (win.every( pos => choice.includes(pos))) {
+                isOn = false;
+                flag = true;
+            };
+        });
+        return flag;
+    };
 
 
     const board = createBoard();
-    const player1 = createPlayer(name1, "X", '<i class="fa-solid fa-x"></i>');
-    const player2 = createPlayer(name2, "O", '<i class="fa-regular fa-circle"></i>');
-    return {player1, player2, playround} ;
+    const playerX = createPlayer("Florent", "X", '<i class="fa-solid fa-x"></i>');
+    const playerO = createPlayer("Computer", "O", '<i class="fa-regular fa-circle"></i>');
+    return {playerX, playerO, playround} ;
 })();
 
 const displayController = (() => {
+
     const boxes = [...document.getElementsByClassName('box')];
+
+    const infoMsg = document.getElementById('message');
+    infoMsg.innerText = "Let's go X Player !"
+
+    let isListen = true;
     
     boxes.forEach( box => {
         box.addEventListener('click', function(event) {
-            let indexBox = boxes.indexOf(box);
-            gameController.playround(indexBox);
-
-            writeSign(box);
-            event.stopPropagation();
-
-        } )
-        
+            if (isListen) {
+                let resultRound = gameController.playround(boxes.indexOf(box));
+                if (resultRound) {
+                    writeSign(box, resultRound.codeSign);
+                    writeMsg(resultRound.nextPlayer.sign);
+                    if (resultRound.winner) {
+                        endGame(resultRound.codeSign);
+                        isListen = false;
+                    }
+                    event.stopPropagation();
+                }
+            }
+        })
     });
 
-    const writeSign = box => {
-        
 
 
-        box.innerHTML = gameController.player1.codeSign;
+
+
+
+
+
+    const writeSign = (box, codeSign) => {
+        box.innerHTML = codeSign;
     };
+
+    const writeMsg = (sign) => {
+        infoMsg.innerText = `Your turn ${sign} Player !`;
+    };
+
+
+
+    const endGame = (sign) => {
+        infoMsg.innerHTML = `The Winner is  ${sign} ! <br> Press F5 to play again !`;
+    };
+
 
 })();
 
